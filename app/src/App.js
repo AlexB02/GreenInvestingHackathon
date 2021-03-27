aea9d60da7a0fbc451fa189098a6720e31a728f9
Luca Mehl <lsm20@ic.ac.uk>
2021-03-27 19:03:02 +0000

    login switching works

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a>alright taylors</a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/logIn">
+          <LogInPage />
+        </Route>
+        <Route path="/signUp">
+          <SignUpPage/>
+        </Route>
+        <Route path="/">
+          <HomePage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+function HomePage() {
+  return (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/signUp">Sign Up</Link>
+      </li>
+      <li>
+        <Link to="/logIn">Log In</Link>
+      </li>
+    </ul>
+  </div>
+  );
+}
+
+function SignUpPage() {
+  return <h2>signup page</h2>;
+}
+
+function LogInPage() {
+  return <h2>Login Page</h2>;
+}
