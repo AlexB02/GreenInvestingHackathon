67d969a7a8846037ffda88c513960e49b6abf766
AlexB.02 <alexanderdb8@gmail.com>
2021-03-28 00:07:31 +0000

    Reformatted portfolioAttrGetters.py

--- backend/stockAnalysis/portfolioAttrGetters.py
+++ backend/stockAnalysis/portfolioAttrGetters.py
@@ -1,21 +1,20 @@
 from stockAttributeGetters import Stock
 
-from currencyEnum import Currency as cur
 from stockHeatEnum import heatCheck
+
 
 class Portfolio():
     def __init__(self, list):
         self.list = list
-    
+
     def portfolio_volatitity(self):
         length_list = len(list)
         total_vol = 0
         for item in self.list:
             stock = Stock(item)
             total_vol += stock.get_volatility
-        return total_vol/length_list
+        return total_vol / length_list
 
-            
     def portfolio_heat_value(self):
         length_list = len(list)
         total_heat = 0
@@ -23,9 +22,9 @@ class Portfolio():
             stock = Stock(item)
             total_heat += stock.get_heatValue
 
-        if total_heat/length_list > 1.10:
+        if total_heat / length_list > 1.10:
             return heatCheck.HOT
-        elif total_heat/length_list < 0.90:
+        elif total_heat / length_list < 0.90:
             return heatCheck.COLD
 
     def portfolio_growth_prospect(self):
@@ -34,7 +33,7 @@ class Portfolio():
         for item in self.list:
             stock = Stock(item)
             total_growth += stock.get_growth_prospect
-        return total_growth/length_list
+        return total_growth / length_list
 
     def portfolio_green_index(self):
         length_list = len(list)
@@ -42,4 +41,4 @@ class Portfolio():
         for item in self.list:
             stock = Stock(item)
             total_green += stock.get_green_index
-        return total_green/length_list
+        return total_green / length_list
